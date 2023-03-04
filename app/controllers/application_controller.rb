fee73c8d4e9d592f8f4806beebe3834c27a5bf0b
colinstrachan <col@strachanmusic.com>
2023-03-04 14:45:33 -0500

    fix redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def ensure_www
     if request.subdomain != 'www'
-      redirect_to url_for(subdomain: 'www', only_path: false)
+      redirect_to url_for(subdomain: 'www', only_path: false, allow_other_host: true)
     end
   end
 end
