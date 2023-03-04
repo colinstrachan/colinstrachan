60eb01f1f254851f9b32f27673df4d2b976bd260
colinstrachan <col@strachanmusic.com>
2023-03-04 14:56:52 -0500

    trying redirect again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :ensure_www
 
   private
 
   def ensure_www
     if request.subdomain != 'www'
-      redirect_to url_for(subdomain: 'www', only_path: false, allow_other_host: true)
+      redirect_to url_for(subdomain: 'www', only_path: false), allow_other_host: true
     end
   end
 end
