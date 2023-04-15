8c6055d20518b3504f99278f7ac8a448824c3d5a
colinstrachan <col@strachanmusic.com>
2023-04-15 10:08:19 -0400

    trying to fix personal site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    richtext = HTTParty.get('http://localhost:1337/api/homepages/1')
   end
 end
