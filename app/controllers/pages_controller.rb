5a5943a57b8ba354d1ca7846a17dfcbc73d7d7d0
colinstrachan <col@strachanmusic.com>
2023-09-23 20:34:48 -0400

    updated site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,30 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @hero = {
+      headline: HTTParty.get('http://localhost:1337/api/homepage?populate=*')['data']['attributes']['hero']['headline'],
+      subhead: HTTParty.get('http://localhost:1337/api/homepage?populate=*')['data']['attributes']['hero']['subhead'],
+      cta: HTTParty.get('http://localhost:1337/api/homepage?populate=*')['data']['attributes']['hero']['cta'],
+      url: HTTParty.get('http://localhost:1337/api/homepage?populate=*')['data']['attributes']['hero']['url'],
+      cover: HTTParty.get('http://localhost:1337/api/homepage?populate=*&populate=hero.cover')['data']['attributes']['hero']['cover']['data']['attributes']['url']
+    }
+    @socials = {
+      github_url: HTTParty.get('http://localhost:1337/api/socials?populate=*')['data'][0]['attributes']['url'],
+      github_icon: HTTParty.get('http://localhost:1337/api/socials?populate=*')['data'][0]['attributes']['icon']['data']['attributes']['url'],
+      github_name: HTTParty.get('http://localhost:1337/api/socials?populate=*')['data'][0]['attributes']['name'],
+      linkedin_url: HTTParty.get('http://localhost:1337/api/socials?populate=*')['data'][1]['attributes']['url'],
+      linkedin_icon: HTTParty.get('http://localhost:1337/api/socials?populate=*')['data'][1]['attributes']['icon']['data']['attributes']['url'],
+      linkedin_name: HTTParty.get('http://localhost:1337/api/socials?populate=*')['data'][1]['attributes']['name']
+    }
+    @bio = {
+      title: HTTParty.get('http://localhost:1337/api/homepage?populate=*&populate=bio.avatar')['data']['attributes']['bio'][0]['title'],
+      content: HTTParty.get('http://localhost:1337/api/homepage?populate=*&populate=bio.avatar')['data']['attributes']['bio'][0]['content'],
+      avatar: HTTParty.get('http://localhost:1337/api/homepage?populate=*&populate=bio.avatar')['data']['attributes']['bio'][0]['avatar']['data']['attributes']['url']
+    }
+  end
+
+  private
+
+  def get_from_strapi(section, element)
   end
 end
